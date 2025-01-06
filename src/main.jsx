3ae2b5ec3bcc76ac4227d09e2f7333f41d0a6afe
LakshyaBagani <Lakshyabagani123@gmail.com>
2025-01-06 14:45:14 +0530

    auth id change

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-       <GoogleOAuthProvider clientId={import.meta.env.VITE_COOCLE_AUTH_CLIENT_ID}>
+       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
            <Headers/>
            <Toaster />
            <RouterProvider router={router} />
